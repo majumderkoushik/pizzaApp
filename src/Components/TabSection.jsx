ecce6d939f3d35b2b0bf2c70104c1a3829927bc5
majumderkoushik <majumderkoushikme@gmail.com>
2023-12-11 04:00:38 +0530

    7th commit

--- src/Components/TabSection.jsx
+++ src/Components/TabSection.jsx
@@ -57,7 +57,7 @@ const TabSection = () => {
 
       <div className="flex items-center justify-center">
         <img
-          src="https://images.unsplash.com/photo-1613564834361-9436948817d1?q=80&w=1943&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          href="https://images.unsplash.com/photo-1613564834361-9436948817d1?q=80&w=1943&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="Delicious Pizza"
           className="w-80 h-auto rounded-full"
         />
